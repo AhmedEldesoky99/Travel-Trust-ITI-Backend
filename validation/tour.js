7f73bec1984d5aefb75c7a0debb1bf65ce5bb5e8
unknown <ahmedeldedoky99@gmail.com>
2023-06-14 18:33:33 +0300

    fix bug

--- validation/tour.js
+++ validation/tour.js
@@ -10,26 +10,26 @@ const validationObj = {
   duration: joi.number().required(),
   start_date: joi.date().required(),
   end_date: joi.date().required(),
-  description: joi.string(),
+  description: joi.string().required(),
   dress_code: joi.string(),
   include: joi.array().items(joi.string()),
   meeting_point: joi.object().keys({
     latitude: joi.number().min(-90).max(90).required(),
     longitude: joi.number().min(-90).max(90).required(),
   }),
-  publish: joi.boolean(),
-  highlight_photos: joi.array().items(joi.object()).required(),
-  food_photos: joi.array().items(joi.object()).required(),
+  publish: joi.boolean().required().default(true),
+  highlight_photos: joi.array().items(joi.string()).required(),
+  food_photos: joi.array().items(joi.string()).required(),
   plan: joi.array().items(
     joi.object().keys({
-      image: joi.object().required(),
+      image: joi.array().items(joi.string()).required(),
       title: joi.string().required(),
       start_time: joi.string().regex(/^((0?[1-9]|1[0-2]):[0-5]\d [ap]m)$/),
       end_time: joi.string().regex(/^((0?[1-9]|1[0-2]):[0-5]\d [ap]m)$/),
       details: joi.array().items(
         joi.object().keys({
           stop_location: joi.string().required(),
-          duration: joi.string().required(),
+          duration: joi.number().required(),
         })
       ),
     })
@@ -39,8 +39,8 @@ const validationObj = {
 const createTourSchema = joi.object(validationObj);
 const updateTourSchema = joi.object({
   ...validationObj,
-  highlight_photos: joi.string(),
-  sale: joi.number(),
+  // highlight_photos: joi.string(),
+  sale: joi.number().min(1).max(99),
 });
 
 const findItemInObj = (itemName, arr) => {
@@ -53,6 +53,7 @@ const createValidTour = async (req, res, next) => {
     await createTourSchema.validateAsync({
       ...req.body,
       highlight_photos: findItemInObj("highlight_photos", req.files),
+      food_photos: findItemInObj("food_photos", req.files),
     });
     next();
   } catch (err) {
@@ -63,6 +64,7 @@ const updateValidTour = async (req, res, next) => {
   try {
     await updateTourSchema.validateAsync({
       ...req.body,
+      food_photos: findItemInObj("food_photos", req.files),
       highlight_photos: findItemInObj("highlight_photos", req.files),
     });
     next();
