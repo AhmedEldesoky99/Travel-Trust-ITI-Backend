aa5b8b7e283707a44b54b5b056028e3895e735d6
unknown <ahmedeldedoky99@gmail.com>
2023-06-21 14:13:49 +0300

    tour stats function

--- controllers/tour/helper.js
+++ controllers/tour/helper.js
@@ -1,3 +1,5 @@
+const { isArray } = require("util");
+
 exports.calcRate = (comments) => {
   const sumRates = comments.reduce((acc, item) => acc + item.rating, 0);
   console.log(sumRates, "sumRates");
@@ -5,4 +7,59 @@ exports.calcRate = (comments) => {
   console.log(length, "length");
 
   return Math.ceil((((sumRates * 100) / length) * 5) / 100);
+};
+
+exports.toursStats = (data) => {
+  let minPrice,
+    maxPrice,
+    categories = {},
+    cities = {},
+    rates = {};
+  data.map((tour) => {
+    minPrice = data[0].price_per_person;
+    maxPrice = data[0].price_per_person;
+
+    if (tour.price_per_person > maxPrice) {
+      maxPrice = tour.price_per_person;
+    } else {
+      minPrice = tour.price_per_person;
+    }
+    if (isArray(tour.category))
+      tour.category?.map((cat) => {
+        categories = {
+          ...categories,
+          [cat.name]: categories[cat.name] ? ++categories[cat.name] : 1,
+        };
+      });
+    else {
+      categories = {
+        ...categories,
+        [tour.category.name]: categories[tour.category.name]
+          ? ++categories[tour.category.name]
+          : 1,
+      };
+    }
+    if (isArray(tour.category))
+      tour.city?.map((cit) => {
+        cities = {
+          ...cities,
+          [cit.title]: cities[cit.title] ? ++cities[cit.title] : 1,
+        };
+      });
+    else {
+      cities = {
+        ...cities,
+        [tour.city.title]: cities[tour.city.title]
+          ? ++cities[tour.city.title]
+          : 1,
+      };
+    }
+    rates = {
+      ...rates,
+      [tour.rate + "Star"]: rates[tour.rate + "Star"]
+        ? ++rates[tour.rate + "Star"]
+        : 1,
+    };
+  });
+  return { minPrice, maxPrice, categories, cities, rates };
 };
--- controllers/tour/toursStats.js
+++ controllers/tour/toursStats.js
@@ -1,70 +1,14 @@
-const { isArray } = require("util");
-const { tourModel: Tour } = require("../../models");
+const { toursStats } = require("./helper");
 
 exports.ToursStats = async (req, res, next) => {
   try {
     const { data } = req.Result;
 
-    const toursStats = () => {
-      let minPrice,
-        maxPrice,
-        categories = {},
-        cities = {},
-        rates = {};
-      data.map((tour) => {
-        minPrice = data[0].price_per_person;
-        maxPrice = data[0].price_per_person;
-
-        if (tour.price_per_person > maxPrice) {
-          maxPrice = tour.price_per_person;
-        } else {
-          minPrice = tour.price_per_person;
-        }
-        if (isArray(tour.category))
-          tour.category?.map((cat) => {
-            categories = {
-              ...categories,
-              [cat.name]: categories[cat.name] ? ++categories[cat.name] : 1,
-            };
-          });
-        else {
-          categories = {
-            ...categories,
-            [tour.category.name]: categories[tour.category.name]
-              ? ++categories[tour.category.name]
-              : 1,
-          };
-        }
-        if (isArray(tour.category))
-          tour.city?.map((cit) => {
-            cities = {
-              ...cities,
-              [cit.title]: cities[cit.title] ? ++cities[cit.title] : 1,
-            };
-          });
-        else {
-          cities = {
-            ...cities,
-            [tour.city.title]: cities[tour.city.title]
-              ? ++cities[tour.city.title]
-              : 1,
-          };
-        }
-        rates = {
-          ...rates,
-          [tour.rate + "Star"]: rates[tour.rate + "Star"]
-            ? ++rates[tour.rate + "Star"]
-            : 1,
-        };
-      });
-      return { minPrice, maxPrice, categories, cities, rates };
-    };
-
     req.Result = {
       ...req.Result,
       length: undefined,
       data: {
-        ...toursStats(),
+        ...toursStats(data),
       },
     };
     next();
