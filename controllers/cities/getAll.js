472df7c71b818ed0a5e2954500802813a970069b
Ahmed Eldesoky <61599746+AhmedEldesoky99@users.noreply.github.com>
2023-06-25 13:52:24 +0300

    Update getAll.js

--- controllers/cities/getAll.js
+++ controllers/cities/getAll.js
@@ -1,4 +1,4 @@
-const { CitiesModal: City } = require("../../models");
+const { CitiesModal: City, tourModel: Tour } = require("../../models");
 const { successHandler } = require("../../utils/responseHandler");
 const { citiesArr } = require("./data");
 
@@ -6,6 +6,18 @@ exports.getAllCities = async (req, res, next) => {
   try {
     //insert cities
     // await City.insertMany(citiesArr);
+
+    const noToursOfCity = await Tour.aggregate([
+      { $group: { _id: "$city", tours_number: { $sum: 1 } } },
+    ]);
+
+    await Promise.all(
+      noToursOfCity.map(async (item) => {
+        await City.findByIdAndUpdate(item._id, {
+          tours_number: item.tours_number,
+        });
+      })
+    );
 
     const cities = await City.find().limit(+req.query.limit);
 
